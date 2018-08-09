99b8c1966dd33c4962a29b131cc0a4d40749c385
hurleyd <donal.hurley@hmhco.com>
2018-08-09 15:42:24 +0100

    update configuration correctly for schema

--- src/main/java/com/example/kafka/KafkaConfig.java
+++ src/main/java/com/example/kafka/KafkaConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class KafkaConfig {
 
-    @Value("${spring.kafka.consumer.properties.schema.registry.url}")
+    @Value("${spring.cloud.stream.schemaRegistryClient.endpoint}")
     private String endpoint;
 
     @Bean
